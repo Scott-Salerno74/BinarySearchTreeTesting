1cd51b31d7aa7877e3a6c78ffbc3bd8ce47c77ce
Scott-Salerno74 <sws001@lvc.edu>
2019-02-08 09:13:31 -0500

    Added JUnit 5 Test for BinarySearchTree

---
+++ TestingProject/src/BinaryTree/BinarySearchTreeTest.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPersonRecordByLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+}
