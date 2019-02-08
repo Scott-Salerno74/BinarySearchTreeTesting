9088b9f451662bc1d80efe71b865bde71347e8d4
ngc2392 <phillicon4733@gmail.com>
2019-02-08 09:49:40 -0500

    added deleting stuff

--- TestingProject/src/UserInterface/Main.java
+++ TestingProject/src/UserInterface/Main.java
@@ -233,7 +233,21 @@ public class Main extends Application {
         insertBox.getChildren().add(insert);
         grid.addRow(18, insertBox);
 
-         insert.setOnAction(new EventHandler<ActionEvent>() {
+        final TextField deleteUserTextField = new TextField();
+        deleteUserTextField.setPromptText("Delete user by last name...");
+        deleteUserTextField.setMaxWidth(300);
+        HBox deleteUserRow = new HBox(10);
+        deleteUserRow.getChildren().add(deleteUserTextField);
+        grid.addRow(19, deleteUserRow);
+
+        Button delete = new Button("Delete");
+        HBox deleteBox = new HBox(100);
+        deleteBox.setAlignment(Pos.BOTTOM_LEFT);
+        deleteBox.getChildren().add(delete);
+        grid.addRow(20, deleteBox);
+
+
+        insert.setOnAction(new EventHandler<ActionEvent>() {
              public void handle(ActionEvent event) {
 
                  System.out.println("Inserting...");
@@ -301,6 +315,6 @@ public class Main extends Application {
         System.out.println(db.getInt());
         System.out.println("done");
 
-        //launch(args);
+        launch(args);
     }
 }
