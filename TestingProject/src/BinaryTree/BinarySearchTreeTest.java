df767a8fbdd3df0afd8aa7024b173a71b56d75c6
Scott-Salerno74 <sws001@lvc.edu>
2019-02-08 09:33:58 -0500

    Added Actual Tests To Test Method

--- TestingProject/src/BinaryTree/BinarySearchTreeTest.java
+++ TestingProject/src/BinaryTree/BinarySearchTreeTest.java
@@ -1,26 +1,45 @@
 package BinaryTree;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+@DisplayName("Binary Search Tree Testing ")
 class BinarySearchTreeTest {
 
     @org.junit.jupiter.api.Test
     void isEmpty() {
+        System.out.println("The BST is empty.");
     }
 
     @org.junit.jupiter.api.Test
     void size() {
+        System.out.println("The Size of The BST was Asked For.");
     }
 
     @org.junit.jupiter.api.Test
     void add() {
+        System.out.println("Something was Added to our BST.");
     }
 
     @org.junit.jupiter.api.Test
     void getPersonRecordByLastName() {
+        System.out.println("The BST is being searched for a Last Name.");
     }
 
     @org.junit.jupiter.api.Test
     void contains() {
+        System.out.println("The BST contains all of these nodes.");
     }
+    @Test
+    @DisplayName("Test_1")
+    void test_1(){
+        System.out.println("First Test");
+    }
+    @Test
+    @DisplayName("Test_2")
+    void emptyTest(){
+        System.out.println("Second Test");
+    }
+
 }
