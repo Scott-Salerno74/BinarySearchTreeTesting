adaad0989e6efe2bf84a37a5aff3700f047b88c5
ngc2392 <phillicon4733@gmail.com>
2019-02-12 15:22:08 -0500

    changes

--- TestingProject/src/MockingTests/MockitoTest.java
+++ TestingProject/src/MockingTests/MockitoTest.java
@@ -2,9 +2,7 @@ package MockingTests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.
 
-@ExtendWith(MockitoExtension.class)
 public class MockitoTest {
 
 
